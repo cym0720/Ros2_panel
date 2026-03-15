77aab76148787b50a036a343589ee4d082084bde
cym0720 <3221444734@qq.com>
2026-03-15 21:49:18 +0800

    问题解决，主要是DDS通信

--- test2/package_srvcli/src/srv.cpp
+++ test2/package_srvcli/src/srv.cpp
@@ -1,39 +1,63 @@
 #include "rclcpp/rclcpp.hpp"
 
 #include <memory>
+#include <exception>
 
 #include "../include/calculator.hpp"
 #include "tutorial_interfaces/srv/prod.hpp"
 #include "tutorial_interfaces/srv/sum.hpp"
 
-class calculator_server : public rclcpp::Node{
-  public:
-    calculator_server()
-    : Node("calculator_server")
-    {
-      calculator_ = std::make_unique<PyCalculator>();
-      RCLCPP_INFO(this->get_logger(), "Calculator server is ready! 1 + 2 +3 = %f", calculator_->add(1, 2, 3));
-      prod_service_ = this->create_service<tutorial_interfaces::srv::Prod>(
-        "prod", 
-        [this](const std::shared_ptr<tutorial_interfaces::srv::Prod::Request> request,
-        std::shared_ptr<tutorial_interfaces::srv::Prod::Response> response) {
-          RCLCPP_INFO(this->get_logger(), "Received request, %f * %f * %f", request->a, request->b, request->c);
-          response->prod = calculator_->multiply(request->a, request->b, request->c);
-        });
-      sum_service_ = this->create_service<tutorial_interfaces::srv::Sum>(
-        "sum", 
-        [this](const std::shared_ptr<tutorial_interfaces::srv::Sum::Request> request,
-        std::shared_ptr<tutorial_interfaces::srv::Sum::Response> response) {
-          RCLCPP_INFO(this->get_logger(), "Received request, %f + %f + %f", request->a, request->b, request->c);
-          response->sum = calculator_->add(request->a, request->b, request->c);
-        });
-      RCLCPP_INFO(this->get_logger(), "Calculator server is ready!");
-    };
+class calculator_server : public rclcpp::Node {
+public:
+  calculator_server()
+  : Node("calculator_server")
+  {
+    calculator_ = std::make_unique<PyCalculator>();
+    RCLCPP_INFO(this->get_logger(), "Calculator init ok: 1 + 2 + 3 = %f",
+                calculator_->add(1, 2, 3));
 
-    private:
-      std::unique_ptr<PyCalculator> calculator_;
-      rclcpp::Service<tutorial_interfaces::srv::Prod>::SharedPtr prod_service_;
-      rclcpp::Service<tutorial_interfaces::srv::Sum>::SharedPtr sum_service_;
+    prod_service_ = this->create_service<tutorial_interfaces::srv::Prod>(
+      "prod",
+      [this](
+        const std::shared_ptr<tutorial_interfaces::srv::Prod::Request> request,
+        std::shared_ptr<tutorial_interfaces::srv::Prod::Response> response) {
+        try {
+          RCLCPP_INFO(this->get_logger(), "Received prod request: %f * %f * %f",
+                      request->a, request->b, request->c);
+          response->prod = calculator_->multiply(request->a, request->b, request->c);
+          RCLCPP_INFO(this->get_logger(), "Send prod response: %f", response->prod);
+        } catch (const std::exception &e) {
+          RCLCPP_ERROR(this->get_logger(), "Prod service failed: %s", e.what());
+          response->prod = 0.0;
+        }
+      });
+
+    RCLCPP_INFO(this->get_logger(), "prod service created");
+
+    sum_service_ = this->create_service<tutorial_interfaces::srv::Sum>(
+      "sum",
+      [this](
+        const std::shared_ptr<tutorial_interfaces::srv::Sum::Request> request,
+        std::shared_ptr<tutorial_interfaces::srv::Sum::Response> response) {
+        try {
+          RCLCPP_INFO(this->get_logger(), "Received sum request: %f + %f + %f",
+                      request->a, request->b, request->c);
+          response->sum = calculator_->add(request->a, request->b, request->c);
+          RCLCPP_INFO(this->get_logger(), "Send sum response: %f", response->sum);
+        } catch (const std::exception &e) {
+          RCLCPP_ERROR(this->get_logger(), "Sum service failed: %s", e.what());
+          response->sum = 0.0;
+        }
+      });
+
+    RCLCPP_INFO(this->get_logger(), "sum service created");
+    RCLCPP_INFO(this->get_logger(), "Calculator server is ready!");
+  }
+
+private:
+  std::unique_ptr<PyCalculator> calculator_;
+  rclcpp::Service<tutorial_interfaces::srv::Prod>::SharedPtr prod_service_;
+  rclcpp::Service<tutorial_interfaces::srv::Sum>::SharedPtr sum_service_;
 };
 
 int main(int argc, char **argv)
@@ -42,4 +66,5 @@ int main(int argc, char **argv)
   auto node = std::make_shared<calculator_server>();
   rclcpp::spin(node);
   rclcpp::shutdown();
+  return 0;
 }
